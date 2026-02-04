52d15c000d0696c1f14c9c9c8f83a71f7d961835
amil <abeshiri.ab@gmail.com>
2026-02-04 07:53:03 -0500

    test

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" | cut -d: -f2  | head -n -2 > 5-password.txt
+john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" | cut -d: -f2 > 5-password.txt
