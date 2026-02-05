5783280e0967f447b5bc229b2036ed5399c42c2b
amil <abeshiri.ab@gmail.com>
2026-02-05 02:58:05 -0500

    test

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 "$1" && john --show --format=raw-sha256 "$1" | awk -F: '{print $2}'  > 6-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 "$1"  > 6-password.txt
