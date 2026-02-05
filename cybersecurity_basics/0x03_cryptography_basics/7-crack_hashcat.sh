a129a53228cc3c54fd1df8370f7a82db4df2ea17
amil <abeshiri.ab@gmail.com>
2026-02-05 03:58:39 -0500

    test

--- cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/7-crack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -m 1400 -a 0 "$1" /user/share/wordlists/rockyou.txt && hashcat -m 1400 --show "$1" | cut -d: -f2 | tr -d '[:space:]' > 7-password.txt
+hashcat -m 1400 -a 0 "$1" /usr/share/wordlists/rockyou.txt && hashcat -m 1400 --show "$1" | cut -d: -f2 | tr -d '[:space:]' > 7-password.txt
