aa57a5183d012be612621fc1af3849370b7c663c
amil <abeshiri.ab@gmail.com>
2026-02-05 04:49:46 -0500

    test

--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -m 0 -a 1 -D 1 "$1" wordlist1.txt wordlist2.txt && hashcat -m 0 --show "$1" | cut -d: -f2 | tr -d '[:space:]' > 9-password.txt
+hashcat -m 0 -a 1 -D 2 "$1" wordlist1.txt wordlist2.txt && hashcat -m 0 --show "$1" | cut -d: -f2 | tr -d '[:space:]' > 9-password.txt
