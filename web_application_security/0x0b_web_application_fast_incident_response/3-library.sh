0e6734de77ddfa51b42612e574b68e93de4c47fb
amil <abeshiri.ab@gmail.com>
2026-04-10 05:33:52 -0400

    file

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep $(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}') $1 | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+awk -F'"' '{print $6}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
