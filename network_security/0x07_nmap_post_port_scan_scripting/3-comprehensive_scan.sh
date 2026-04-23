308d320965fa5d23c67c6e01ad52dbd52b8e4464
amil <abeshiri.ab@gmail.com>
2026-04-23 09:31:44 -0400

    file

--- network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/3-comprehensive_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sC -sV --script http-vuln-cve2017-5638,ssl-enum-ciphers -p 443,ftp-anon $1 -oN comprehensive_scan_results.txt
+nmap -sC -sV -p443 --script http-vuln-cve2017-5638,ssl-enum-ciphers,ftp-anon $1 -oN comprehensive_scan_results.txt
