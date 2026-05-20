8976e5ae33850ad9867dc6a7210dc3468857a2a3
amil <abeshiri.ab@gmail.com>
2026-05-20 08:55:14 -0400

    file

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,22 +1,18 @@
 #!/usr/bin/env ruby
-require "json"
+
+require 'json'
+
 def count_user_ids(path)
-  file_content = File.read(path)
-  data = JSON.parse(file.content)
+  file = File.read(path)
+  data = JSON.parse(file)
 
-  id_counts = Hash.new(0)
+  counts = Hash.new(0)
 
-  if data.is_indexable? && data.is_a?(Array)
-    data.each do |item|
-      if item['userId']
-        user_id = item['userId']
-        id_counts[user_id] += 1
-      end
-    end
-  elsif data.is_a?(Hash) && data['userId']
-    user_id = data['userId']
-    id_counts[user_id] += 1
+  data.each do |item|
+    counts[item["userId"]] += 1
   end
 
-  id_counts
+  counts.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
 end
